3eda00933b98cd28ef32733e7b319309c54a0846
Locke <moyerock@gmail.com>
2016-10-26 11:21:31 +0800

    chore:

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 (function() {
   var debug = require('debug')('consul-sdk');
+  var path = require('path')
 
   debug('__dirname', __dirname);
   debug('__filename', __filename);
   debug('process.cwd()', process.cwd());
+  debug('path.resolve', path.resolve('./'));
 })();
